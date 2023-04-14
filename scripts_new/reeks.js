3bd12ed837a65ac84a409758f71df2ed3a50f0b9
BeckersTom <Tom.Beckers@gmail.com>
2023-04-14 18:07:41 +0200

    Website test to use new xmls

---
+++ scripts_new/reeks.js
@@ -0,0 +1,121 @@
+// global queryparametersmap
+var param_list;
+
+// handle onreadystatechange event of req object
+function processReqChange() {
+    // only if req shows "loaded"
+    if (req.readyState == 4) {
+        // only if "OK"
+        if (req.status == 200) {
+            insertXMLData();
+         } else {
+            alert("There was a problem retrieving the XML data:\n" +
+                req.statusText);
+         }
+    }
+}
+
+// display details retrieved from XML document
+function insertXMLData() {
+    var div = document.getElementById("hoofdtekst"); 
+    //var header = document.createElement("h1");
+    //var headercontent = document.createTextNode("Strip Info");
+    //header.appendChild(headercontent);
+    //div.appendChild(header);
+    var reeksen = req.responseXML.getElementsByTagName("Reeks");
+    for (var i = 0; i < reeksen.length; i++) {
+       var reeks_titel = document.createTextNode(getElementTextNS("", "Titel", reeksen[i], 0));
+       var reeksenheader = document.createElement("h2");
+       reeksenheader.appendChild(reeks_titel);
+       div.appendChild(reeksenheader);
+       var strips = getChildrenByTagName(reeksen[i],"Strip");
+	   var table = document.createElement("table");
+       for (var j = 0; j < strips.length; j++) {         
+          var tr = document.createElement("tr");
+		  var nummer = document.createTextNode(getElementTextNS("", "Nummer", strips[j], 0));
+		  var td_nummer = document.createElement("td");
+		  td_nummer.setAttribute("class","stripnumber");
+		  td_nummer.appendChild(nummer);
+		  var titel  = document.createTextNode(getElementTextNS("", "Titel", strips[j], 0));
+		  var td_titel = document.createElement("td");
+		  td_titel.appendChild(titel);
+		  var aanwezig = getElementTextNS("", "Aanwezig", strips[j], 0);
+		  var td_aanwezig = document.createElement("td");
+		  td_aanwezig.setAttribute("class","boolean");
+		  if (aanwezig == 1) {
+		    var img_green = document.createElement('img'); 
+			img_green.src = 'images/green.png';
+			img_green.alt = 'x';
+			img_green.setAttribute("class","boolean");
+			td_aanwezig.appendChild(img_green);
+		  } else {
+			var img_red = document.createElement('img'); 
+			img_red.src = 'images/red.png';
+			img_red.alt = 'x';
+			img_red.setAttribute("class","boolean");
+			td_aanwezig.appendChild(img_red);
+		  }
+		  var gelezen = getElementTextNS("", "Gelezen", strips[j], 0);
+		  var td_gelezen = document.createElement("td");
+		  td_gelezen.setAttribute("class","boolean");
+		  if (gelezen == 1) {
+		    var img_green = document.createElement('img'); 
+			img_green.src = 'images/green.png';
+			img_green.alt = 'x';
+			img_green.setAttribute("class","boolean");
+			td_gelezen.appendChild(img_green);
+		  } else {
+			var img_red = document.createElement('img'); 
+			img_red.src = 'images/red.png';
+			img_red.alt = 'x';
+			img_red.setAttribute("class","boolean");
+			td_gelezen.appendChild(img_red);
+		  }
+		  var id = document.createTextNode(getElementTextNS("", "ID", strips[j], 0));
+		  var td_id = document.createElement("td");
+		  td_id.setAttribute("class","stripid");
+		  td_id.appendChild(id);
+		  tr.appendChild(td_nummer);
+		  tr.appendChild(td_titel);
+		  tr.appendChild(td_aanwezig);
+		  tr.appendChild(td_gelezen);
+		  tr.appendChild(td_id);
+		  table.appendChild(tr);
+          div.appendChild(table);
+       }  
+    }
+    //DEBUG IE
+    var debuginfo = document.createElement("xmp");
+    var debuginfocontent = document.createTextNode("Debuginfo : " + div.innerHTML);
+    debuginfo.appendChild(debuginfocontent);
+    // div.appendChild(debuginfo);
+    //END DEBUG IE
+}
+
+// display details retrieved from XML document
+function loadXMLData(doc) {
+    try {
+       loadXMLDoc(doc);
+    }
+    catch(e) {
+       var msg = (typeof e == "string") ? e : ((e.message) ? e.message : "Unknown Error");
+       alert("Unable to get XML data:\n" + msg);
+       return;
+    }
+}
+
+function loadXMLDataForReeks() {
+	var id = '19';
+	var url = window.location.toString();
+	var query_string = url.split("?");
+	var params = query_string[1].split("&");
+	for (var i = 0; i < params.length; i++) {	
+		var param_item = params[0].split("=");
+		if (param_item[0] == 'id')
+		{
+			id = param_item[1];
+		}
+	}
+	var reeks_xml = 'xml/strips/reeks_' + id + '.xml';
+	loadXMLData(reeks_xml);
+}
---
+++ scripts_new/reeksen.js
@@ -0,0 +1,55 @@
+// handle onreadystatechange event of req object
+function processReqChange() {
+    // only if req shows "loaded"
+    if (req.readyState == 4) {
+        // only if "OK"
+        if (req.status == 200) {
+            insertXMLData();
+         } else {
+            alert("There was a problem retrieving the XML data:\n" +
+                req.statusText);
+         }
+    }
+}
+
+// display details retrieved from XML document
+function insertXMLData() {
+    var div = document.getElementById("hoofdtekst"); 
+    var header = document.createElement("h1");
+    var headercontent = document.createTextNode("Reeksen");
+    header.appendChild(headercontent);
+    div.appendChild(header);
+	var hr_elem = document.createElement("hr");
+	hr_elem.setAttribute("class","wide");
+	div.appendChild(hr_elem);
+    var reeksen = req.responseXML.getElementsByTagName("Reeks");
+    for (var i = 0; i < reeksen.length; i++) {
+	   var reeks_para = document.createElement("p");
+	   var reeks_id = getElementTextNS("", "ID", reeksen[i], 0);
+	   var reeks_href = 'reeks.html?id=' + reeks_id;
+       var reeks_titel = document.createTextNode(getElementTextNS("", "Titel", reeksen[i], 0));
+	   var reeks_anchor = document.createElement("a");
+	   reeks_anchor.appendChild(reeks_titel);
+       reeks_anchor.href = reeks_href;
+	   reeks_para.appendChild(reeks_anchor);
+       div.appendChild(reeks_para);
+    }
+    //DEBUG IE
+    //var debuginfo = document.createElement("xmp");
+    //var debuginfocontent = document.createTextNode("Debuginfo : " + div.innerHTML);
+    //debuginfo.appendChild(debuginfocontent);
+    // div.appendChild(debuginfo);
+    //END DEBUG IE
+}
+
+// display details retrieved from XML document
+function loadXMLData(doc) {
+    try {
+       loadXMLDoc(doc);
+    }
+    catch(e) {
+       var msg = (typeof e == "string") ? e : ((e.message) ? e.message : "Unknown Error");
+       alert("Unable to get XML data:\n" + msg);
+       return;
+    }
+}
